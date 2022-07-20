024cb1504d0eb59cd01a310f404b0a2cedb86486
Sergey Lisoviy <serlipaua@gmail.com>
2022-07-20 13:36:01 +0300

    fix 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,56 +1,48 @@
 import throttle from 'lodash.throttle';
 
 const feedbackForm = document.querySelector('.feedback-form');
-const userData = {};
+let userData = {};
 const STORAGE_USER_DATA_KEY = 'feedback-form-state';
 
 document.addEventListener('load', onLoad());
 feedbackForm.addEventListener('input', throttle(onInput, 500));
 feedbackForm.addEventListener('submit', onSubmit);
 
-// console.log(localStorage.getItem(STORAGE_USER_DATA_KEY));
+console.log('now in storage', localStorage.getItem(STORAGE_USER_DATA_KEY));
 
 function onInput(e) {
   createUserDataField(e);
   saveLocalData(userData);
-  //   console.log(e.currentTarget);
-
-  //   const {
-  //     elements: { email, message },
-  //   } = e.currentTarget;
-
-  //   userData[e.target.name] = e.target.value;
-
-  //   console.log(userData);
-
-  //   localStorage.setItem(STORAGE_USER_DATA_KEY, JSON.stringify(userData));
-
-  //   console.log(localStorage.getItem(STORAGE_USER_DATA_KEY));
 }
 
 function onLoad() {
-  console.log('Load page - OK');
+  // console.log('Load page - OK');
   const loadedData = loadLocalData();
-  if (!loadedData) {
-    console.log('No data in local storage in KEY - ', STORAGE_USER_DATA_KEY);
-    return;
+  if (loadedData) {
+    feedbackForm.email.value = loadedData.email;
+    feedbackForm.message.value = loadedData.message;
+    userData.email = loadedData.email;
+    userData.message = loadedData.message;
+    console.log('user data ', userData);
   }
-  feedbackForm.email.value = loadedData.email;
-  feedbackForm.message.value = loadedData.message;
-  console.log('on load', feedbackForm.email);
+
+  // console.log('on load', feedbackForm.email);
 }
 
 function createUserDataField(e) {
   userData[e.target.name] = e.target.value;
+  // console.log('user data ', userData);
 }
 
 function saveLocalData(data) {
   localStorage.setItem(STORAGE_USER_DATA_KEY, JSON.stringify(data));
+  console.log('in storage: ', localStorage.getItem(STORAGE_USER_DATA_KEY));
 }
 
 function loadLocalData() {
   try {
     const loadedData = JSON.parse(localStorage.getItem(STORAGE_USER_DATA_KEY));
+
     return loadedData;
   } catch (error) {
     console.log(error.message);
@@ -62,5 +54,6 @@ function onSubmit(e) {
   const loadedData = loadLocalData();
   localStorage.clear();
   feedbackForm.reset();
+  userData = {};
   console.log('Loaded user data :', loadedData);
 }
